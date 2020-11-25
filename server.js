bae041530aa6a85d2d671a989bb294e903408ec5
Zain <reply2zain@gmail.com>
2020-11-24 23:58:33 -0500

    minor update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
-app.listen(3008)
-
+// app.listen(3008)
+app.listen(process.env.PORT || 3000)
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers',
